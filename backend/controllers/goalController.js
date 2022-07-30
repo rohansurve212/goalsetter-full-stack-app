b7d2fff6389ad6384b485ab927fc0517de807eca
Rohan Surve <rohansurve212@gmail.com>
2022-07-30 18:00:26 -0400

    Goals controller updated

--- backend/controllers/goalController.js
+++ backend/controllers/goalController.js
@@ -1,32 +1,34 @@
 /** @format */
+import asyncHandler from "express-async-handler";
+
 // @desc    Get goals
 // @route   GET /api/goals
 // @access  Private
-export const getGoals = async (req, res) => {
+export const getGoals = asyncHandler(async (req, res) => {
   res.status(200).json({ message: "Get goals" });
-};
+});
 
 // @desc    Set goal
 // @route   POST /api/goals
 // @access  Private
-export const setGoal = async (req, res) => {
+export const setGoal = asyncHandler(async (req, res) => {
   if (!req.body.text) {
     res.status(400);
     throw new Error("Please add a text field");
   }
   res.status(200).json({ message: "Set goal" });
-};
+});
 
 // @desc    Update goal
 // @route   PUT /api/goals/:id
 // @access  Private
-export const updateGoal = async (req, res) => {
+export const updateGoal = asyncHandler(async (req, res) => {
   res.status(200).json({ message: `Update goal ${req.params.id}` });
-};
+});
 
 // @desc    Delete goal
 // @route   DELETE /api/goals/:id
 // @access  Private
-export const deleteGoal = async (req, res) => {
+export const deleteGoal = asyncHandler(async (req, res) => {
   res.status(200).json({ message: `Delete goal ${req.params.id}` });
-};
+});
